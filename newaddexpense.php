0fc8a7c1889d5fbbe2459f292b62c20855965ff3
usowmya <35606837+usowmya@users.noreply.github.com>
2018-07-05 09:58:35 -0300

    add expense page
    add expense page

---
+++ newaddexpense.php
@@ -0,0 +1,157 @@
+<html xmlns:padding="http://www.w3.org/1999/xhtml">
+<head>
+    <!-- REFERENCES:
+    BootStrap : https://getbootstrap.com/docs/4.1
+    Image: https://goo.gl/images/iK8YWf
+    javascript: https://www.w3schools.com/jsref/prop_email_multiple.asp
+    Header: https://stackoverflow.com/questions/18712338/make-header-and-footer-files-to-be-included-in-multiple-html-pages
+  -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./bootstrap-4.1.1-dist/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="./bootstrap-4.1.1-dist/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="./addexpense.css"/>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.0.4/popper.js"></script>
+    <script src="bootstrap-4.1.1-dist/js/bootstrap.min.js"></script>
+    <title> Split Expense Page </title>
+</head>
+<body>
+
+<!-- Nav Bar -->
+<nav class="navbar navbar-expand-sm navbar-light bg-light">
+    <a class="navbar-brand" href="#">
+        <!-- Brand Logo created using Photoshop -->
+        <img src="Image/weblogo.png" class="logo" alt="brand">
+    </a>
+    <div class="toggle">
+        <button class="navbar-toggler hidden-sm-up" type="button" data-toggle="collapse" data-target="#collapseContents" aria-controls="collapseContents" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+    </div>
+
+
+    <div class="collapse navbar-collapse navbar-toggleable-xs " id="collapseContents">
+
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0 ">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#about">About</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Services
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#services">Add Expense</a>
+                    <a class="dropdown-item" href="#services">Set Budget Limit</a>
+                    <a class="dropdown-item" href="#services">Split Expense</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#services">Others</a>
+                </div>
+            </li>
+        </ul>
+        <div class="nav-item navbar px-2">
+            <button class="btn btn-outline-success" data-toggle="modal" data-target="#login-modal">Login or Register</button>
+        </div>
+    </div>
+</nav>
+
+<div id="header"></div><br/>
+<div class="container-fluid">
+    <div class="card bg-dark text-white">
+<!--        <img class="card-img" style="-webkit-filter: blur(3px); filter: blur(3px);" src="./images/splitBg.jpg" alt="Card image cap">-->
+        <div class="card-img-overlay">
+            <div class="container-fluid" align="center" >
+                <div class="card" style="max-width: 40%; color:#0E2658; opacity: 0.8; padding:10px">
+                    <h4><b> Add Expense</b></h4>
+                    <div class="card-body" style="max-width: 100%;"  padding:10px; align="left">
+                        <form data-toggle="validator" role="form">
+
+                            <div class="input-group mb-3">
+                                <div class="input-group-prepend">
+                                    <label class="input-group-text" for="inputGroupSelect01">Category</label>
+                                </div>
+                                <select class="custom-select" id="inputGroupSelect01">
+                                    <option selected>select a category</option>
+                                    <option value="1">Food</option>
+                                    <option value="2">Groceries</option>
+                                    <option value="3">WIFI</option>
+                                    <option value="4">Electricity</option>
+                                    <option value="5">Rent</option>
+                                    <option value="6">General</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label class="control-label col-xs-3" for="Amount">Amount</label>
+                                <div class="col-xs-9">
+                                    <input type="number" class="form-control" name="amount" id="amount" placeholder="Enter Amount">
+                                </div>
+
+                                <div class="form-check mb-2 mr-sm-2">
+                                    <input class="form-check-input" type="checkbox" id="inlineFormCheck">
+                                    <label class="form-check-label" for="inlineFormCheck">
+                                        Split Bill
+                                    </label>
+                                </div>
+
+<!--                                <label class="control-label col-xs-3" for="inputEmail">Email IDs</label>-->
+<!--                                <div class="col-xs-9">-->
+<!--                                    <input type="email" class="form-control"  name="inputEmail" id="inputEmail" placeholder="Email IDs">-->
+<!--                                </div>-->
+                            </div>
+                            <div class="form-group">
+                                <div class="form-row">
+                                    <div class="col">
+                                        <button type="button" class="btn btn-primary" onclick="window.location.href='./index.html'" style="background-color:#0E2658;"> Cancel </button>
+                                    </div>
+                                    <div class="col">
+                                        <button type="submit" class="btn btn-primary" onclick="return myFunction()" style="background-color:#0E2658;"> Add </button>
+                                    </div>
+                                </div>
+                            </div>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    function myFunction() {
+        document.getElementById("inputEmail").multiple = true;
+        var email =  document.getElementById("inputEmail").value;
+        var amount = document.getElementById("amount").value;
+        if(email.length == 0)
+        {
+            alert('Please enter Email ID');
+            return false;
+        }
+        if(amount.length == 0)
+        {
+            alert('Amount Can not be empty');
+            return false;
+        }
+        return true;
+        document.getElementById("demo").innerHTML = "The email field now accept multiple values.";
+    }
+</script>
+<script>
+    $("#header").load("./header.html");
+</script>
+</body>
+</html>
+
+
+
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sowmya Umesh
+ * Date: 7/4/2018
+ * Time: 2:26 PM
+ */
